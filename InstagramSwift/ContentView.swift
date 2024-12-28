a3713902d66ccef9980089f93af50533c7aca2e8
Robin Perez <35049749+rperezalberto@users.noreply.github.com>
2024-12-28 19:47:55 -0400

    Initial Commit

---
+++ InstagramSwift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InstagramSwift
+//
+//  Created by Robin Perez on 12/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramSwift/InstagramSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftApp.swift
+//  InstagramSwift
+//
+//  Created by Robin Perez on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
